d549fdc7928e31f554c6251849fa3b692bcbca36
Enhoo <37933161+Enhoo@users.noreply.github.com>
2021-06-01 13:59:54 +0800

    Update config.js

--- tourism/utils/upload/config.js
+++ tourism/utils/upload/config.js
@@ -1,9 +1,9 @@
-let fileHost = "http://hatu-cdn.oss-cn-hangzhou.aliyuncs.com/"
+let fileHost = ""
 let config = {
 	//aliyun OSS config
 	uploadImageUrl: `${fileHost}`, //默认存在根目录，可根据需求改
-	AccessKeySecret: 'Ieyvf7dwNKbImtsJOF2w32BN64rNBX',
-	OSSAccessKeyId: 'LTAI4FzGpbK5C8FkfJbv13gS',
+	AccessKeySecret: '',
+	OSSAccessKeyId: '',
 	timeout: 87600 //这个是上传文件时Policy的失效时间
 };
 module.exports = config
